954b213f462ebc43f8e4b5a75c122d0edc4ef7d4
Martin Masevski <martin.masevski@gmail.com>
2017-12-19 10:22:25 +0100

    check first email

--- register.php
+++ register.php
@@ -16,16 +16,7 @@ if(isset($_POST['password']) && isset($_POST['psw-repeat']) && isset($_POST['ema
     $passwordConfirm = $_POST['psw-repeat'];
     $email = $_POST['email'];
     
-    if(strlen($password) < 8){/*check password lenght*/
-		echo 'Password is too short. Use at least 8 char';
-        $error = true;
-        $connection->close();
-        
-	}else if($password != $passwordConfirm){/*check password confirm*/
-		echo 'Password check do not match.';
-        $error = true;
-        $connection->close();
-    } else if(filter_var($email,FILTER_VALIDATE_EMAIL)){/*check valid email*/
+    if(filter_var($email,FILTER_VALIDATE_EMAIL)){/*check valid email*/
         $emailAlreadyUsed ="SELECT u_id FROM user WHERE email='$email';";
         $result = $connection->query($emailAlreadyUsed);
         if ($result->num_rows > 0){/*check email already used*/
@@ -35,12 +26,23 @@ if(isset($_POST['password']) && isset($_POST['psw-repeat']) && isset($_POST['ema
         }
     }else{
         echo 'Use valid email';
+		$error = true;
+        $connection->close();
+    }else if(strlen($password) < 8){/*check password lenght*/
+		echo 'Password is too short. Use at least 8 char';
+        $error = true;
+        $connection->close();
+        
+	}else if($password != $passwordConfirm){/*check password confirm*/
+		echo 'Password check do not match.';
+        $error = true;
+        $connection->close();
     }
 	
 	if(!$error){
 		
 		$pass_hash = secured_hash($password);
-    	$insertQuery = "INSERT INTO `my_wavesound`.`utente` (`email`, `pass`) VALUES ('$email', '$pass_hash');";
+    	$insertQuery = "INSERT INTO `my_wavesound`.`user` (`email`, `password`) VALUES ('$email', '$pass_hash');";
     	$result = $connection->query($insertQuery);
 		echo 'Data send succesfully to DB. Check your DB';
 	} else {
