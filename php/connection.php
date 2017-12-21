3c23ecb04309ada54ca952de935c1804ca5080af
Martin Masevski <martin.masevski@gmail.com>
2017-12-21 12:59:22 +0100

    Nuova struttura
    php separato, link relativi

---
+++ php/connection.php
@@ -0,0 +1,17 @@
+<?php
+
+$dbhost = 'localhost';
+$dbuser = 'wavesound';
+$dbpassword = '';
+$dbname = 'my_wavesound';
+
+// Create connection
+$connection = mysqli_connect($dbhost, $dbuser, $dbpassword, $dbname);
+
+// Check connection
+if (!$connection) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
+
+?>
