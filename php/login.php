3db6a61bc45b093c6f29b8d34d91ac7a6a760dd6
MartinG711 <giacomomartinello711@gmail.com>
2017-12-22 14:27:25 +0100

    Fix Login

---
+++ php/login.php
@@ -0,0 +1,37 @@
+<?php
+
+require('connection.php');
+
+if(isset($_POST['email']) && isset($_POST['password'])){
+		
+	/*save varialble*/
+	$password = $_POST['password'];
+	$email = $_POST['email'];
+	
+	/*find out password*/
+	$queryPass = "SELECT password FROM user WHERE email='$email'";
+	$result = $connection->query($queryPass);
+	
+	if($result->num_rows == 1){
+		$row = mysqli_fetch_row($result);
+		
+		/*check password if exist in db*/
+		if(password_verify($password,$row[0])){
+			echo "uguali";
+			/*find out username*/
+			$queryUser = "SELECT username FROM user WHERE email='$email'";
+			$res = $connection->query($queryUser);
+			$user = mysqli_fetch_row($res);
+			session_start();
+			$_SESSION["username"] = $user[0];
+		}else{
+			echo "diversi";
+		}
+	}else{
+		echo "non trovato";
+	}
+	
+	
+}
+
+?>
