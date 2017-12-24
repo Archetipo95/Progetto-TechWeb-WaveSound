1819f5a820b0093467ae6f541068f1c5c14e03ec
Martin Masevski <martin.masevski@gmail.com>
2017-12-24 10:08:37 +0100

    Aggiunta su tutti i file

--- php/checkSession.php
+++ php/checkSession.php
@@ -1,6 +1,8 @@
 <?php
 	session_start();
-               if(!isset($_SESSION['username'])){
-                header("location:index.html");
-        }
+    if(!isset($_SESSION['username'])){
+    	header("location:index.html");
+    }else{
+		header("location:logout.php");
+	}
 ?>
