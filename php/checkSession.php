101605d996ea0c5b65905403c24b4de1af7ebbcd
Martin Masevski <martin.masevski@gmail.com>
2017-12-24 10:15:57 +0100

    piccolo errore di redirect

--- php/checkSession.php
+++ php/checkSession.php
@@ -1,8 +1,6 @@
 <?php
 	session_start();
     if(!isset($_SESSION['username'])){
-    	header("location:index.html");
-    }else{
-		header("location:logout.php");
-	}
+    	header("location:logout.php");
+    }
 ?>
