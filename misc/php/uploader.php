cb5800126a63df3edae0f2f651413fc258b3f9e9
Martin Masevski <martin.masevski@gmail.com>
2018-01-06 16:45:38 +0100

    fixed error messages

--- misc/php/uploader.php
+++ misc/php/uploader.php
@@ -1,7 +1,7 @@
 <?php
 
 require('checkSession.php');
-
+require('msg.php');
 
 
 $target_dir = "../img/users/";
@@ -13,31 +13,31 @@ $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 if(isset($_POST["submit"])) {
     $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
     if($check !== false) {
-        echo "File is an image - " . $check["mime"] . ".";
+        sendMessage("File is an image - " . $check["mime"] . ".");
         $uploadOk = 1;
     } else {
-        echo "File is not an image.";
+        sendMessage("File is not an image.");
         $uploadOk = 0;
     }
 }
 // Check if file already exists
 if (file_exists($target_file)) {
-    echo "Sorry, file already exists.";
+    sendMessage("Sorry, file already exists.");
     $uploadOk = 0;
 }
 // Check file size
 if ($_FILES["fileToUpload"]["size"] > 500000) {
-    echo "Sorry, your file is too large.";
+    sendMessage("Sorry, your file is too large.");
     $uploadOk = 0;
 }
 // Allow certain file formats
 if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg") {
-    echo "Sorry, only JPG, JPEG & PNG files are allowed.";
+    sendMessage("Sorry, only JPG, JPEG & PNG files are allowed.");
     $uploadOk = 0;
 }
 // Check if $uploadOk is set to 0 by an error
 if ($uploadOk == 0) {
-    echo "Sorry, your file was not uploaded.";
+    sendMessage("Sorry, your file was not uploaded.");
 // if everything is ok, try to upload file
 } else {
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
@@ -45,12 +45,13 @@ if ($uploadOk == 0) {
 		$_SESSION["avatar"] = basename( $_FILES["fileToUpload"]["name"]);
 		/*aggiornare db*/
 		require('connection.php');
-		$avata = $_SESSION["avatar"];
-		$idi = $_SESSION["userID"];
-		$updateAvatar = "UPDATE user SET avatar = '$avata' WHERE u_id = '$idi' ";
+		$avatar = $_SESSION["avatar"];
+		$userID = $_SESSION["userID"];
+		$updateAvatar = "UPDATE user SET avatar = '$avatar' WHERE u_id = '$userID' ";
 		$result = $connection->query($updateAvatar);
+		sendMessage("Your avatar image was updated");
     } else {
-        echo "Sorry, there was an error uploading your file.";
+        sendMessage("Sorry, there was an error uploading your file.");
     }
 }
 ?>
