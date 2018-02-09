58811687abb43d8e9505a1c350873b9ef5ab81ab
Martin Masevski <martin.masevski@gmail.com>
2018-02-09 10:37:15 +0100

    Aggiornamento

--- misc/php/connection.php
+++ misc/php/connection.php
@@ -5,14 +5,14 @@ $dbpassword = '';
 $dbname = 'my_wavesound';
 
 try {
-    $connection = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpassword);
-    // set the PDO error mode to exception
-    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    //echo "Connected successfully"; 
-    }
+	$connection = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpassword);
+	// set the PDO error mode to exception
+	$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	//echo "Connected successfully";
+	}
 catch(PDOException $e)
-    {
-    echo "Connection failed: " . $e->getMessage();
-    }
+	{
+	echo "Connection failed: " . $e->getMessage();
+	}
 
 ?>
