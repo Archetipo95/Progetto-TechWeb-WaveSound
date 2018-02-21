97f31aa72fd6550cfdc1012e6cb901a58fc8fef1
Martin Masevski <martin.masevski@gmail.com>
2018-02-21 19:41:57 +0100

    moved jump-button in header

--- misc/js/jump-top.js
+++ misc/js/jump-top.js
@@ -4,9 +4,9 @@ window.onscroll = function () {
 
 function scrollFunction() {
 	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-		document.getElementById("button-top").style.display = "block";
+		document.getElementById("jump-top").style.display = "block";
 	} else {
-		document.getElementById("button-top").style.display = "none";
+		document.getElementById("jump-top").style.display = "none";
 	}
 }
 
