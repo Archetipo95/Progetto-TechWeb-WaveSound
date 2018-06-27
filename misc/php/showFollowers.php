76be1e55d1b6de8030783fcbc312e5f9eb4c3265
MartinG711 <giacomomartinello711@gmail.com>
2018-06-27 14:03:43 +0200

    fixed follow system

--- misc/php/showFollowers.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $userFollow = $_GET["userID"];
-    $followers = select("SELECT id_follow FROM follow WHERE id_follow = '$userFollow'");
-    $numfollowers = count($followers);
-?>
-
-
--- misc/php/unfollow.php
+++ misc/php/unfollow.php
@@ -4,5 +4,5 @@
     $follower = $_SESSION["userID"];
     $followed = $_POST["unfollow"];
     delete("DELETE FROM follow WHERE id_follow = '$followed' AND id_user = '$follower'");
-    redirect('..\..\user.html?userID='.$_POST["unfollow"]);
+    redirect('..\..\user.html?userID='.$followed);
 ?>
