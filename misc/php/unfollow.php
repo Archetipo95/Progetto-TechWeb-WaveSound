7e99b6c348ff54296bba31ba2f5cc5a91dd1b9ed
MartinG711 <giacomomartinello711@gmail.com>
2018-06-28 09:10:28 +0200

    restyle follow

--- misc/php/unfollow.php
+++
@@ -1,8 +0,0 @@
-<?php
-    session_start();
-    require('tools.php');
-    $follower = $_SESSION["userID"];
-    $followed = $_POST["unfollow"];
-    delete("DELETE FROM follow WHERE id_follow = '$followed' AND id_user = '$follower'");
-    redirect('..\..\user.html?userID='.$followed);
-?>
