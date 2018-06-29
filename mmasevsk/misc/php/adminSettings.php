e7435d2a71087926203e88d650a45a33d65068fd
MartinG711 <giacomomartinello711@gmail.com>
2018-06-29 13:29:43 +0200

    WIP delete user

--- mmasevsk/misc/php/adminSettings.php
+++ mmasevsk/misc/php/adminSettings.php
@@ -6,18 +6,41 @@
     function deleteAccount() {
         $user = $_POST['user'];
         require('connection.php');
-        $query  = "DELETE FROM user WHERE username='$user' ";
-        /*
-            cosa eliminare
-            user
-            library
-            song
-            commenti
-            likes
-            reported comments
-            follow
-        */
-        $result = $connection->query($query);
+		
+		$keyOff = "SET FOREIGN_KEY_CHECKS=0";
+		$keyOn = "SET FOREIGN_KEY_CHECKS=1";
+		
+		$IDuser = select("SELECT u_id FROM user WHERE username='$user'");
+		echo $IDuser;
+		$RSKeyOff = $connection->query($keyOff);
+		
+		$deleteComm = "DELETE FROM comment WHERE comment.u_id='$IDuser'";
+		$resComm = $connection->query($deleteComm);
+		$deleteLike = "DELETE FROM likes WHERE likes.u_id='$IDuser'";
+		$resLike = $connection->query($deleteLike);
+		
+		$songs = select("SELECT id_song FROM library WHERE id_user='$IDuser'");
+		$deleteLibrary = "DELETE FROM library WHERE library.id_user='$IDuser'";
+		$resLibrary = $connection->query($deleteLibrary);
+		
+		for($i=0;$i<count($songs);$i++){
+			$deleteSong = "DELETE FROM song where id_song='$songs[$i]'";
+			$resultSong = $connection->query($deleteSong);
+		}
+		
+		$deleteFollow = "DELETE FROM follow WHERE id_user='$IDuser' OR id_follow='$IDuser'";
+		$resFollow = $connection->query($deleteFollow);
+		
+		$deleteRepComment = "DELETE FROM reported_comments WHERE id_reporter='$IDuser'";
+		$resRComment = $connection->query($deleteRepComment);
+		
+		$deleteUSB = "DELETE FROM user_email_banned WHERE admin_id='$Iduser'";
+		$resUSB = $connection->query($deleteUSB);
+		
+        $query  = "DELETE FROM user WHERE u_id='$IDuser'";
+		$res = $connection->query($query);
+		
+		$RSKeyOn = $connection->query($keyOn);
         sendMessage("$user account deleted");
     }
     
